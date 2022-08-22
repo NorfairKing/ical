8f0a3da040c77f0c8afbd7c48c593be2cabb8457
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-08-22 16:51:30 +0200

    combinator for scenario dir tests for components

--- ical-gen/src/ICal/Component/Gen.hs
+++ ical-gen/src/ICal/Component/Gen.hs
@@ -4,12 +4,15 @@
 
 module ICal.Component.Gen where
 
+import Control.Arrow (left)
+import qualified Data.ByteString as SB
 import qualified Data.DList as DList
 import Data.GenValidity
 import Data.GenValidity.CaseInsensitive ()
 import Data.GenValidity.Text ()
 import Data.GenValidity.Time ()
 import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import ICal.Component
 import ICal.ContentLine
 import ICal.ContentLine.Gen ()
@@ -35,6 +38,35 @@ instance GenValid Standard
 instance GenValid Daylight
 
 instance GenValid TimeZone
+
+componentScenarioDir ::
+  forall a.
+  (Show a, Eq a, Validity a, IsComponent a) =>
+  FilePath ->
+  Spec
+componentScenarioDir dir = scenarioDir dir $ \tzFile ->
+  it "can parse this file as a timezone and roundtrip it" $ do
+    let parseBS bs = do
+          textContents <- left show $ TE.decodeUtf8' bs
+          unfoldedLines <- parseUnfoldedLines textContents
+          contentLines <- mapM parseContentLineFromUnfoldedLine unfoldedLines
+          parseComponentFromContentLines contentLines
+
+        renderBS =
+          TE.encodeUtf8
+            . renderContentLines
+            . DList.toList
+            . componentSectionB
+
+    contents <- SB.readFile tzFile
+    case parseBS contents of
+      Left err -> expectationFailure err
+      Right result -> do
+        shouldBeValid (result :: a)
+        let rendered = renderBS result
+        case parseBS rendered of
+          Left err -> expectationFailure err
+          Right result' -> result' `shouldBe` result
 
 componentSpec ::
   forall a.
--- ical-gen/test/ICal/TimeZoneSpec.hs
+++ ical-gen/test/ICal/TimeZoneSpec.hs
@@ -1,37 +1,11 @@
+{-# LANGUAGE TypeApplications #-}
+
 module ICal.TimeZoneSpec (spec) where
 
-import Control.Arrow (left)
-import qualified Data.ByteString as SB
-import qualified Data.DList as DList
-import qualified Data.Text.Encoding as TE
 import ICal.Component
-import ICal.ContentLine
-import ICal.UnfoldedLine
+import ICal.Component.Gen
 import Test.Syd
-import Test.Syd.Validity
 
 spec :: Spec
 spec = do
-  scenarioDir "test_resources/timezones" $ \tzFile ->
-    it "can parse this file as a timezone and roundtrip it" $ do
-      let parseBS bs = do
-            textContents <- left show $ TE.decodeUtf8' bs
-            unfoldedLines <- parseUnfoldedLines textContents
-            contentLines <- mapM parseContentLineFromUnfoldedLine unfoldedLines
-            parseComponentFromContentLines contentLines
-
-          renderBS =
-            TE.encodeUtf8
-              . renderContentLines
-              . DList.toList
-              . componentSectionB
-
-      contents <- SB.readFile tzFile
-      case parseBS contents of
-        Left err -> expectationFailure err
-        Right result -> do
-          shouldBeValid (result :: TimeZone)
-          let rendered = renderBS result
-          case parseBS rendered of
-            Left err -> expectationFailure err
-            Right result' -> result' `shouldBe` result
+  componentScenarioDir @TimeZone "test_resources/timezones"
