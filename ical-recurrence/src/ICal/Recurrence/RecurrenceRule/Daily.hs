6308b0abf569a3c412c72c571fb267ecd5fde679
Tom Sydney Kerckhove <syd@cs-syd.eu>
2022-10-03 03:39:01 +0200

    Daily recurrence

--- ical-recurrence/src/ICal/Recurrence/RecurrenceRule/Daily.hs
+++ ical-recurrence/src/ICal/Recurrence/RecurrenceRule/Daily.hs
@@ -3,9 +3,11 @@ module ICal.Recurrence.RecurrenceRule.Daily
   )
 where
 
+import Control.Monad
 import Data.Set (Set)
 import Data.Time as Time
 import ICal.PropertyType
+import ICal.Recurrence.RecurrenceRule.Util
 
 dailyDateTimeRecurrence ::
   Day ->
@@ -19,4 +21,42 @@ dailyDateTimeRecurrence ::
   Set BySecond ->
   Set BySetPos ->
   [LocalTime]
-dailyDateTimeRecurrence = undefined
+dailyDateTimeRecurrence
+  limit
+  lt@(LocalTime d_ tod_)
+  interval
+  byMonths
+  byMonthDays
+  byDays
+  byHours
+  byMinutes
+  bySeconds
+  bySetPoss = do
+    d <- dailyDayRecurrence d_ limit interval byMonths byMonthDays byDays
+    tod <- filterSetPos bySetPoss $ timeOfDayExpand tod_ byHours byMinutes bySeconds
+    let next = LocalTime d tod
+    guard (next > lt) -- Don't take the current one again
+    guard (next < LocalTime (addDays 1 limit) midnight) -- Don't go beyond the limit
+    pure next
+
+-- | Internal: Get all the relevant days until the limit, not considering any 'Set BySetPos'
+dailyDayRecurrence ::
+  Day ->
+  Day ->
+  Interval ->
+  Set ByMonth ->
+  Set ByMonthDay ->
+  Set DayOfWeek ->
+  [Day]
+dailyDayRecurrence
+  d_
+  limitDay
+  (Interval interval)
+  byMonths
+  byMonthDays
+  byDays = do
+    d <- takeWhile (<= limitDay) $ map (\i -> addDays (fromIntegral interval * i) d_) [0 ..]
+    guard $ byMonthLimit byMonths d
+    guard $ byMonthDayLimit byMonthDays d
+    guard $ byEveryWeekDayLimit byDays d
+    pure d
