44512f36d969a2ccb38499bc2382f7c1b7a151c8
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-05-30 16:20:16 +0200

    parameter tests

--- ical-gen/test/ICal/ParameterSpec.hs
+++ ical-gen/test/ICal/ParameterSpec.hs
@@ -24,6 +24,41 @@ spec = do
     -- @
     parameterExampleSpec ["John Smith"] (CommonName "John Smith")
 
+    -- @
+    -- ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=TENTATIVE;CN=Henry
+    --  Cabot:mailto:hcabot@example.com
+    -- ATTENDEE;ROLE=REQ-PARTICIPANT;DELEGATED-FROM="mailto:bob@
+    --  example.com";PARTSTAT=ACCEPTED;CN=Jane Doe:mailto:jdoe@
+    --  example.com
+    -- @
+    parameterExampleSpec ["Henry Cabot"] (CommonName "Henry Cabot")
+    parameterExampleSpec ["Jane Doe"] (CommonName "Jane Doe")
+    -- @
+    -- ATTENDEE;CN=John Smith;DIR="ldap://example.com:6666/o=ABC%
+    --  20Industries,c=US???(cn=Jim%20Dolittle)":mailto:jimdo@
+    --  example.com
+    -- @
+    parameterExampleSpec ["John Smith"] (CommonName "John Smith")
+
+    -- @
+    -- ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=TENTATIVE;DELEGATED-FROM=
+    --  "mailto:iamboss@example.com";CN=Henry Cabot:mailto:hcabot@
+    --  example.com
+    -- ATTENDEE;ROLE=NON-PARTICIPANT;PARTSTAT=DELEGATED;DELEGATED-TO=
+    --  "mailto:hcabot@example.com";CN=The Big Cheese:mailto:iamboss
+    --  @example.com
+    -- ATTENDEE;ROLE=REQ-PARTICIPANT;PARTSTAT=ACCEPTED;CN=Jane Doe
+    --  :mailto:jdoe@example.com
+    -- @
+    parameterExampleSpec ["The Big Cheese"] (CommonName "The Big Cheese")
+    -- @
+    --     ORGANIZER;CN=John Smith:mailto:jsmith@example.com
+    --
+    --     ORGANIZER;CN=JohnSmith;DIR="ldap://example.com:6666/o=DC%20Ass
+    --      ociates,c=US???(cn=John%20Smith)":mailto:jsmith@example.com
+    -- @
+    parameterExampleSpec ["JohnSmith"] (CommonName "JohnSmith")
+
   describe "Encoding" $ do
     genValidSpec @Encoding
     parameterSpec @Encoding
@@ -32,6 +67,7 @@ spec = do
     --   ATTACH;FMTTYPE=text/plain;ENCODING=BASE64;VALUE=BINARY:TG9yZW
     -- @
     parameterExampleSpec ["BASE64"] EncodingBase64
+    parameterExampleSpec ["8BIT"] Encoding8Bit
 
   describe "FormatType" $ do
     genValidSpec @FormatType
@@ -45,6 +81,36 @@ spec = do
     -- @
     parameterExampleSpec ["application/msword"] (FormatType "application/msword")
 
+    -- @
+    -- ATTACH;FMTTYPE=application/postscript:ftp://example.com/pub/
+    --  reports/r-960812.ps
+    -- @
+    parameterExampleSpec ["application/postscript"] (FormatType "application/postscript")
+
+    -- @
+    -- X-ABC-MMSUBJ;VALUE=URI;FMTTYPE=audio/basic:http://www.example.
+    --  org/mysubj.au
+    -- @
+    parameterExampleSpec ["audio/basic"] (FormatType "audio/basic")
+
+    -- @
+    -- ATTACH;FMTTYPE=text/plain;ENCODING=BASE64;VALUE=BINARY:VGhlIH
+    --  F1aWNrIGJyb3duIGZveCBqdW1wcyBvdmVyIHRoZSBsYXp5IGRvZy4
+    -- @
+    parameterExampleSpec ["text/plain"] (FormatType "text/plain")
+    -- @
+    -- ATTACH;FMTTYPE=image/vnd.microsoft.icon;ENCODING=BASE64;VALUE
+    --  =BINARY:AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAA
+    --  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAACAAAAAgIAAAICAgADAwMAA////AAAA
+    --  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    --  AAAAAAAAAAAAAAAAAAAAAAMwAAAAAAABNEMQAAAAAAAkQgAAAAAAJEREQgAA
+    --  ACECQ0QgEgAAQxQzM0E0AABERCRCREQAADRDJEJEQwAAAhA0QwEQAAAAAERE
+    --  AAAAAAAAREQAAAAAAAAkQgAAAAAAAAMgAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    --  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+    --  AAAAAAAAAAAA
+    -- @
+    parameterExampleSpec ["image/vnd.microsoft.icon"] (FormatType "image/vnd.microsoft.icon")
+
   describe "ParticipationStatus" $ do
     genValidSpec @ParticipationStatus
     parameterSpec @ParticipationStatus
@@ -56,6 +122,8 @@ spec = do
     -- @
     parameterExampleSpec ["ACCEPTED"] ParticipationStatusAccepted
     parameterExampleSpec ["DECLINED"] ParticipationStatusDeclined
+    parameterExampleSpec ["TENTATIVE"] ParticipationStatusTentative
+    parameterExampleSpec ["DELEGATED"] ParticipationStatusDelegated
 
   describe "RSVPExpectation" $ do
     genValidSpec @RSVPExpectation
